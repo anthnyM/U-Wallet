f7fa2397dbb7d6d90402f48ae2baa86acd127d88
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2024-10-23 23:16:34 -0400

    modify cypress in gastador

--- cypress/e2e/gastador.cy.js
+++ cypress/e2e/gastador.cy.js
@@ -1,14 +1,18 @@
-describe("Gastador", () => {
-    it("Muestra el gasto registrado", () => {
-      cy.visit("/");
+describe("Gestión de Gastos", () => {
+  it("Debería registrar y mostrar gastos con descripción", () => {
+      cy.visit("/Gastos.html");
+
       cy.get("#gasto").type(50);
-      cy.get("#registrar-gasto").click();
+      cy.get("#descripcion-gasto").type("Compra de comida");
+      cy.get("input[type='submit']").click();
 
       cy.get("#gasto").type(100);
-      cy.get("#registrar-gasto").click();
+      cy.get("#descripcion-gasto").type("Transporte");
+      cy.get("input[type='submit']").click();
 
       cy.get("#gastos-div").should("contain", "50");
+      cy.get("#gastos-div").should("contain", "Compra de comida");
       cy.get("#gastos-div").should("contain", "100");
-    });
+      cy.get("#gastos-div").should("contain", "Transporte");
   });
-  
+});
